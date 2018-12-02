440440f8cf85c3bb5f9a6b7d516b4815d46317e5
Ron Klinkien <ron@cyberjunky.nl>
2018-12-02 15:19:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
-import os
-import sys
-
-from setuptools import setup, find_packages
-
-setup(name='plugwiselib', 
-    version='0.2',
-    description='A library for communicating with Plugwise smartplugs',
-    author='Sven Petai',
-    author_email='hadara@bsd.ee',
-    url='https://bitbucket.org/hadara/python-plugwise/wiki/Home',
-    license='MIT',
-    packages=find_packages(),
-    py_modules=['plugwise'],
-    install_requires=[
-          'crcmod',
-          'pyserial',
-       ],
-    download_url='https://github.com/cyberjunky/python-plugwise/archive/0.2.tar.gz',
-    scripts=['plugwise_util'],
-    classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'Topic :: Software Development :: Build Tools',
-      'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python :: 3',
-    ],
+from distutils.core import setup
+setup(
+  name='plugwise',
+  packages=['plugwise'],
+  version='0.2',
+  license='MIT',
+  description='A library for communicating with Plugwise smartplugs',
+  author='Sven Petai',
+  author_email='hadara@bsd.ee',
+  url='https://bitbucket.org/hadara/python-plugwise/wiki/Home',
+  download_url='https://github.com/cyberjunky/python-plugwise/archive/0.2.tar.gz',
+  install_requires=[
+        'crcmod',
+        'pyserial',
+     ],
+  scripts=['plugwise_util'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
 )
 
